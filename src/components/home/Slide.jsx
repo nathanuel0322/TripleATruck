be236b5f093c6c6214a100e4e46a54ab5c7306af
Nathanuel Dixon <nathan0322@hotmail.com>
2022-12-17 00:36:48 -0500

    fixed img height

--- src/components/home/Slide.jsx
+++ src/components/home/Slide.jsx
@@ -2,13 +2,13 @@ import { useState, useEffect } from 'react';
 import '../../assets/css/slide.css'
 
 export default function Slide({image}) {
-    const [slideel, setSlideel] = useState(0)
-    useEffect(() => {
-        setSlideel(document.getElementsByClassName("midslideshow")[0].offsetHeight)
-    }, [])
+    // const [slideel, setSlideel] = useState(0)
+    // useEffect(() => {
+    //     setSlideel(document.getElementsByClassName("midslideshow")[0].offsetHeight)
+    // }, [])
     return (
         <div style={{display:'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', width: '100vw'}}>
-            <img src={require(`../../assets/images/${image}`)} style={{ height: slideel, width: '100%', objectFit: 'cover', filter: 'brightness(30%)'}}  alt='slideimage' />
+            <img src={require(`../../assets/images/${image}`)} style={{ height: 'calc(100vh - 4.875rem)', width: '100%', objectFit: 'cover', filter: 'brightness(30%)'}}  alt='slideimage' />
             <p className='aaatitle' style={{position: 'absolute', alignSelf: 'center', color: 'white', fontFamily: "'Oswald', sans-serif", fontSize: '3.5rem'}}>AAA GAMING</p>
         </div>
     )
