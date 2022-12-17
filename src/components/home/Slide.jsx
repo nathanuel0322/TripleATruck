963a16f0e3ada20c101f7a5f2e01e04da64312d1
Nathanuel Dixon <nathan0322@hotmail.com>
2022-12-17 00:37:24 -0500

    slide org

--- src/components/home/Slide.jsx
+++ src/components/home/Slide.jsx
@@ -1,11 +1,6 @@
-import { useState, useEffect } from 'react';
 import '../../assets/css/slide.css'
 
 export default function Slide({image}) {
-    // const [slideel, setSlideel] = useState(0)
-    // useEffect(() => {
-    //     setSlideel(document.getElementsByClassName("midslideshow")[0].offsetHeight)
-    // }, [])
     return (
         <div style={{display:'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', width: '100vw'}}>
             <img src={require(`../../assets/images/${image}`)} style={{ height: 'calc(100vh - 4.875rem)', width: '100%', objectFit: 'cover', filter: 'brightness(30%)'}}  alt='slideimage' />
