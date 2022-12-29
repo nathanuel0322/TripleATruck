0c2676bf9f95eca5d2415bf74be8fd28ee8a2ce7
Nathanuel Dixon <nathan0322@hotmail.com>
2022-12-29 12:44:20 -0500

    hide site progress

--- src/App.js
+++ src/App.js
@@ -22,20 +22,25 @@ function App() {
   const hamburgerdetector = useMediaQuery({query: '(max-width: 767px)'});
   const [drawerOpen, setDrawerOpen] = useState(false);
   return (
-    <Router>
-        {/* {hamburgerdetector ? <HamburgerNav /> : <Navbar />} */}
-        <Header drawerfunc={setDrawerOpen} />
-        <Routes>
-          <Route exact path='' element={<Home />} />
-          <Route exact path='/packages' element={<Packages />} />
-          <Route exact path='/services' element={<Services />}/>
-          <Route exact path='/gamelist' element={<Gamelist />} />
-          {/* <Route exact path='/contactpage' element={<Contact />} />
-          <Route exact path='/aboutpage' element={<About />} />
-          <Route exact path='/BdayCard' element={<BDayCard />} /> */}
-        </Routes>
-      <TemporaryDrawer drawerstate={drawerOpen} drawerfunc={setDrawerOpen} />
-    </Router>
+    <div style={{margin: '0 auto', color: 'white', height: '100vh', textAlign: 'center', display: 'flex', flexDirection: 'column', 
+      fontFamily: "system-ui", alignItems: 'center', justifyContent: 'center', fontSize: '4rem'}}
+    >
+      Site in Progress 👷‍♂️
+    </div>
+    // <Router>
+    //     {/* {hamburgerdetector ? <HamburgerNav /> : <Navbar />} */}
+    //     <Header drawerfunc={setDrawerOpen} />
+    //     <Routes>
+    //       <Route exact path='' element={<Home />} />
+    //       <Route exact path='/packages' element={<Packages />} />
+    //       <Route exact path='/services' element={<Services />}/>
+    //       <Route exact path='/gamelist' element={<Gamelist />} />
+    //       {/* <Route exact path='/contactpage' element={<Contact />} />
+    //       <Route exact path='/aboutpage' element={<About />} />
+    //       <Route exact path='/BdayCard' element={<BDayCard />} /> */}
+    //     </Routes>
+    //   <TemporaryDrawer drawerstate={drawerOpen} drawerfunc={setDrawerOpen} />
+    // </Router>
   )
 }
 
