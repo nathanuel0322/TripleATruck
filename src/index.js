86b963b9fdd2004937513cd8f7aea4c6031949e3
hammadrashid <hammad.rashid1996@gmail.com>
2024-11-04 17:35:46 +0500

    Configure Google Analytics, Install React-GA4 library

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-EYZTLFDTGH");
+
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
